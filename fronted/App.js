3b2bf8bd1262cd353b4db23d79674075d837d311
SakuragiH4namichi <mellowmellow3t@gmail.com>
2024-10-01 16:14:05 +0900

    made a funciton that displays Tweet in Top

--- fronted/App.js
+++ fronted/App.js
@@ -1,13 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Signin } from "./pages/Signin";
+import { Top } from "./pages/Top";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path={`/`} element={<Signin />} />
+        <Route path={`/top/`} element={<Top />}/>
       </Routes>
     </BrowserRouter>
   );
--- fronted/services/api.ts
+++ fronted/services/api.ts
@@ -1,5 +1,6 @@
 import axios,{ AxiosResponse } from 'axios';
 import { SigninInputs,LoginResponse } from '../types/auth';
+import { TweetResponseDTO } from 'types/tweet';
 
 const api = axios.create({
     baseURL: 'http://localhost:8080/api',
@@ -8,4 +9,8 @@ const api = axios.create({
 
 export const login = async (data: SigninInputs): Promise<AxiosResponse<LoginResponse>> => {
     return await api.post<LoginResponse>('/login', data);
+}
+
+export const getTweetList = async (): Promise<AxiosResponse<TweetResponseDTO[]>> => {
+    return await api.get<TweetResponseDTO[]>('/tweets');
 }
